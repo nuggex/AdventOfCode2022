45830ab113b8563bebcf85f321ee2bb944f3523d
Johannes Edgren <edgrenjo@arcada.fi>
2022-12-02 19:39:23 +0200

    fix

--- Day2/Day2.php
+++ Day2/Day2.php
@@ -10,7 +10,7 @@ class Day2
     public $conv;
     public function __construct()
     {
-        $this->f = file(dirname(__FILE__) . "/i.txt", FILE_IGNORE_NEW_LINES);
+        //$this->f = file(dirname(__FILE__) . "/i.txt", FILE_IGNORE_NEW_LINES);
         $this->p1 = array("X" => 1, "Y" => 2, "Z" => 3);
         $this->p2 = array("A" => 1, "B" => 2, "C" => 3);
         $this->score = array(0 => 3, -1 => 6, 2 => 6, 1 => 0, -2 => 0);
@@ -19,13 +19,16 @@ class Day2
             "Y" => array(1 => 1, 2 => 2, 3 => 3),
             "Z" => array(1 => 2, 3 => 1, 2 => 3)
         );
+        $f = fopen(dirname(__FILE__). "/i.txt", "r");
+        while($x = fgets($f)){
+            $this->f[] =  $x;
+        }
     }
 
     public function part1()
     {
         $temp = 0;
         foreach ($this->f as $e) {
-//            $t = explode(" ", $e);
             $temp += $this->score[$this->p2[$e[0]] - ($p1 = $this->p1[$e[2]])] + $p1;
         }
         return $temp;
@@ -35,7 +38,6 @@ class Day2
     {
         $temp = 0;
         foreach ($this->f as $e) {
-//            $t = explode(" ", $e);
             $temp += $this->score[($p1Value = $this->p2[$e[0]]) - ($val = $this->conv[$e[2]][$p1Value])] + $val;
         }
         return $temp;
