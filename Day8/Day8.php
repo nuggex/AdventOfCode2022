2bc3284944f8b3b535704f6a9738767a2ab447a6
Johannes Edgren <nugge@arcada.fi>
2022-12-08 20:48:51 +0200

    D8 try 2

--- Day8/Day8.php
+++ Day8/Day8.php
@@ -27,41 +27,77 @@ class Day8
         $this->LoadData();
         $trees = 0;
         $trees += (count($this->matrix) * 2) + ((count($this->matrix[0]) - 2) * 2);
-        $counter = 1;
+        $counter = 0;
         $counted = [];
         foreach ($this->matrix as $key => $row) {
             if ($key > 0 && $key < (count($this->matrix) - 1)) {
-                $count = false;
-
                 foreach ($row as $k => $r) {
-                    if ( $k < (count($row)) - 1) {
-                        while ($counter < count($this->matrix) - 1) {
-                            $count = true;
-                            $keyC = $key + $counter;
-                            $kC = $k + $counter;
-                            $keyM = $key - $counter;
-                            $kM = $k - $counter;
-                            if (($keyM > 0) &&
-                                ($kM > 0) &&
-                                ($keyC < (count($this->matrix))) &&
-                                ($kC < (count($row)))) {
-                                if (
-                                    $r < $this->matrix[$keyM][$kM] ||
-                                        $r < $this->matrix[$keyC][$kC] ||
-                                        $r < $this->matrix[$keyM][$kC] ||
-                                        $r < $this->matrix[$keyC][$kM] ) {
-                                    $count = false;
-                                }
+                    /* while ($counter <= count($this->matrix)) {
+                         $count = true;
+                         $keyC = $key + $counter;
+                         $kC = $k + $counter;
+                         $keyM = $key - $counter;
+                         $kM = $k - $counter;
+                         if (($keyM > 0) &&
+                             ($kM > 0) &&
+                             ($keyC <= (count($this->matrix))) &&
+                             ($kC <= (count($row)))) {
+                             if (
+                                 $r < $this->matrix[$keyM][$kM] ||
+                                 $r < $this->matrix[$keyC][$kC] ||
+                                 $r < $this->matrix[$keyM][$kC] ||
+                                 $r < $this->matrix[$keyC][$kM]) {
+                                 echo $k . " - " . $r . "\n";
+                                 $count = false;
+                             }
+                         }
+                         $counter++;
+                     }*/
+                    /*
+                    $counter = 1;
+                    while ($counter <= count($row)) {
+                        $count = true;
+                        $keyYP = $key + $counter;
+                        $kXP = $k + $counter;
+                        $keyXM = $key - $counter;
+                        $kXM = $k - $counter;
+                        if (($keyXM <= (count($this->matrix))) &&
+                            ($kXM <= (count($row)))) {
+                            if (
+                                $r < $this->matrix[$keyXM][$kXM]){
+                                echo $k . " - " . $r . "\n";
+                                $count = false;
                             }
-                            $counter++;
                         }
-                        $counter = 1;
+                        $counter++;
+                    }*/
+
+                    foreach ($this->matrix as $kr => $rr) {
+                        $count = true;
+                        print_r("key: " . $key . " k:" . $k . " kr:" . $kr . "\n");
+                        if ($k > 0 && $k < count($rr)-1 && $kr > 0 && $kr < count($row)-1) {
+                            $keykr = "X" . $key . "-" . $kr;
+                            $krkey = "Y" . $kr . "-" . $key;
+                            if (in_array($keykr, $counted) || in_array($krkey, $counted)) {
+                                $count = false;
+                            }
+                            if ($r < $this->matrix[$kr][$k]) {
+                                $count = false;
+                                $counted[] = $krkey;
+                            }
+                            if($r < $this->matrix[$k][$kr]){
+                                $counted[] = $keykr;
+                                $count = false;
+                            }
+                        }
                     }
-                    if ($count) {
+
+                    if (isset($count) && $count) {
                         $trees++;
                     }
                 }
             }
+
         }
         var_dump($trees);
     }
